85720516b7248b5d419cfc6b34e36e5ee6d6fdb1
Will Gee <williamgee3@hotmail.com>
2024-06-09 20:04:35 -0400

    character-generator

---
+++ character-generator/script.js
@@ -0,0 +1,59 @@
+/*
+  Pragmatic JavaScript
+  Chapter 1
+  Programming Assignment
+
+  Author:
+  Date:
+  Filename:
+*/
+
+"use strict";
+
+function createCharacter(name, gender, characterClass) {
+  // TODO: Implement this function
+}
+
+document.getElementById("generateHero").addEventListener("click", function(e) {
+  e.preventDefault();
+
+  // TODO: Get form values
+  let heroName = document.getElementById("heroName").value;
+  let heroGender = document.getElementById("heroGender").value;
+  let heroClass = document.getElementById("heroClass").value;
+
+  let character = createCharacter(heroName,heroGender,heroClass);
+
+  console.log(character.getName());
+
+  
+
+  // TODO: Create character
+  function createCharacter(name, gender, heroClass) {
+    //console.log(heroClass, gender, name);
+
+    return {
+      getName: function() {
+        return name;
+      },
+
+      getGender: function() {
+        return gender;
+      },
+
+      getClass: function() {
+        return heroClass;
+      }
+    }
+  }
+
+  // TODO: Display character information
+  let outputName = document.getElementById("outputName");
+  outputName.innerHTML = character.getName();
+
+  let outputGender = document.getElementById("outputGender");
+  outputGender.innerHTML = character.getGender();
+
+  let outputClass = document.getElementById("outputClass");
+  outputClass.innerHTML = character.getClass();
+});
